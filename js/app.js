76f5c539cbb6fadee38c9c0957de98922075f849
kejodu <kevindunn_@hotmail.com>
2016-08-26 13:35:17 +0300

    changes for the lightbox

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 var $overlay = $('<div id="overlay"></div>');
 var $image = $('<img>');
-
+var $caption = $('<p></p>');
 
 //Add an overlay
 $("body").append($overlay);
   //An image
   $overlay.append($image);
   //A caption
-
+  $overlay.append($caption);
 
 //Capture the click event on a link to an image
 $(".gallery-list a").click(function(event){
@@ -20,7 +20,8 @@ $(".gallery-list a").click(function(event){
   //Show the overlay
   $overlay.show();
   //Get child's alt attribute and set caption
-
+  var imageCaption = $(this).children("img").attr("alt");
+  $caption.text(imageCaption);
 });
   
 
